b9f962ea9c53348fff7f12c7d0974fce419a7737
Sam <samuel.waters@gmail.com>
2023-03-14 09:17:05 -0600

    chore:finished day 2

--- day-2/proj.py
+++ day-2/proj.py
@@ -4,4 +4,6 @@ tip_percent = int(input("What percentage tip would you like to give? 10, 12, or 
 people = int(input("How many people are splitting this bill? "))
 tip_percent = tip_percent + 1
 bill_total = round(bill * tip_percent, 2)
-print(bill_total / people)
+bill_total_by_person = "{:.2f}".format(bill_total / people)
+# make sure we have two 0 ^^
+print(f"Each person should pay {bill_total_by_person}")
