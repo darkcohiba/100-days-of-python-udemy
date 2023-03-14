7451eb65820ff6e9db266da7f2041251b6844c46
Sam <samuel.waters@gmail.com>
2023-03-14 07:31:01 -0600

    chore:day 1 project

--- day-1/main.py
+++ day-1/main.py
@@ -1,1 +1,38 @@
 print("hello world!")
+
+# lab 1 solution
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# lab 2 solution
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# getting input
+userName = input("What is your username: ")
+print(f"Thank you for playing our game {userName}")
+
+# lab 3 solution
+#Write your code below this line 👇
+
+
+name = input("What is your name? ")
+print(len(name))
+
+# optimal solution
+print(len(input("What is your name? ")))
+
+
+
+
+
+
+
+
+
+
