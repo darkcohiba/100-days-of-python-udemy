2962c8931aa1b9af74b896a31193a8a6ae2804bc
Sam <samuel.waters@gmail.com>
2023-03-14 14:26:27 -0600

    chore: day 2 lab 1

---
+++ day-3/main.py
@@ -0,0 +1,12 @@
+# lab 1
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if (number % 2 != 0):
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+
+# lab 2
