46e2b407ddeb5110b7e29dfa17baddbb8ef65553
Sam <samuel.waters@gmail.com>
2023-03-14 07:41:35 -0600

    chore:added project and last solution

---
+++ day-1/proj.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+homeTown = input("What is your home town? ")
+pet = input("What is your pet's name? ")
+print(f"Hello, {name}, Your band name could be: {homeTown} {pet}")
