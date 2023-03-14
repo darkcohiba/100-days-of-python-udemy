55d2908070c9a12fdccf505f2d43039a0a6c9824
Sam <samuel.waters@gmail.com>
2023-03-14 08:24:56 -0600

    chore:day 2 two labs done

---
+++ day-2/main.py
@@ -0,0 +1,18 @@
+# lab 1 data types
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+# lab 2 converting
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = (int(weight) / (float(height) * float(height)))
+print(int(bmi))
