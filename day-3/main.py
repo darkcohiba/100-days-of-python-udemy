b28e2a7584f4b8e4d432c529f5484e070907bb28
Sam <samuel.waters@gmail.com>
2023-03-14 15:16:14 -0600

    chore:day-2 lab-2

--- day-3/main.py
+++ day-3/main.py
@@ -10,3 +10,28 @@ else:
     print("This is an even number.")
 
 # lab 2
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = (int(weight) / (float(height) * float(height)))
+bmi = round(bmi, 2)
+# print(bmi)
+
+if bmi > 35:
+    bmi = round(bmi)
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+elif 30 < bmi < 35:
+    bmi = round(bmi)
+    print(f"Your BMI is {bmi}, you are obese.")
+elif 25 < bmi < 30:
+    bmi = round(bmi)
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif 18.5 < bmi < 25:
+    bmi = round(bmi)
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+else:
+    bmi = round(bmi)
+    print(f"Your BMI is {bmi}, you are underweight.")
