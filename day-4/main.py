d67adb80332243adaa58f1a3bf41928497fb24de
Sam <samuel.waters@gmail.com>
2023-03-15 11:04:25 -0600

    chore:day-4-lab-1

---
+++ day-4/main.py
@@ -0,0 +1,18 @@
+import random
+
+# random number
+random_interger = random.randint(0,100)
+print (round(random.random(), 2) * 100)
+
+# lab 1
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+random_num = random.randint(0,1)
+if random_num == 1:
+    print ("Heads")
+else:
+    print("Tails")
+
