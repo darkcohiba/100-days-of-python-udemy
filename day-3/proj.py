3c64ad10f9e4ef50eb1bb2bd2b8a29e76d32cd0a
Sam <samuel.waters@gmail.com>
2023-03-15 07:31:04 -0600

    chore:day -3 project

---
+++ day-3/proj.py
@@ -0,0 +1,14 @@
+name = input("What is your name: \n")
+print(f"Hello {name}!")
+print("Welcome to Treasure Island.")
+print("My name is dobbins and I will be accompanying you along your journey!")
+print("Our mission is to find the treasure.")
+first_turn = input("You are at a crossroads... Should we go left or right? \n")
+if first_turn == "left":
+    swim = input("We are at a small river crossing, we notice a small dock up ahead. Should we wait or swim across? swim or wait? \n")
+    if swim == "swim":
+        print(f"A sea snake jumps out of the water, his tail whips back. The sudden crack slices through {name}'s arm leaving him gushing blood. Dobbins swam back and escaped to safety as {name} drowned deep into the ocean.")
+    else:
+        print("As you approach the dock, the boat keeper comes out from the fog. \n Dobbins walks up to him and asks about passage across the river. He gestures you and ")
+else:
+    print("Sadly a group alligators was hidden in the bushes and jumped out the second we turned right. Game Over...")
