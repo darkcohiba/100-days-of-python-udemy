7389656742bc52a43c040a026be55d93fcb52bc9
Sam <samuel.waters@gmail.com>
2023-03-15 07:52:30 -0600

    chore:day -3 project finished

--- day-3/proj.py
+++ day-3/proj.py
@@ -10,7 +10,7 @@ if first_turn == "left":
         print(f"A sea snake jumps out of the water, his tail whips back. The sudden crack slices through {name}'s arm leaving him gushing blood. Dobbins swam back and escaped to safety as {name} drowned deep into the ocean... game over...")
     else:
         print("As you approach the dock, the boat keeper comes out from the fog. \n Dobbins walks up to him and asks about passage across the river. He gestures you.")
-        boat = input("As you walk slowly towards him you can feel the air thicken and the stench of the alcohol on his collar grew stronger. Dobbins looks up at you and says 'he can give us passage for a fee, should we do it?', you reluctantly look down at Dobbins and say... 'yes' or 'no'")
+        boat = input("As you walk slowly towards him you can feel the air thicken and the stench of the alcohol on his collar grew stronger. Dobbins looks up at you and says 'he can give us passage for a fee, should we do it?', you reluctantly look down at Dobbins and say... 'yes' or 'no' \n")
         if boat == "yes":
             print("You get on the rickety boat and make it across the water safely.")
         else:
