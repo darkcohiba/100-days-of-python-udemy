b30e0675e0f0a665ebbe20c661a5c121bcdcc74b
Sam <samuel.waters@gmail.com>
2023-03-16 08:28:22 -0600

    chore: day -4-lab-3

--- day-4/main.py
+++ day-4/main.py
@@ -32,3 +32,30 @@ name_length = len(names) - 1
 random_number = random.randint(0, name_length)
 payer = names[random_number]
 print(f"{payer} is going to buy the meal today!")
+
+
+# lab 3
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+
+position = list(position)
+row = (int(position[1]) - 1)
+column = (int(position[0]) - 1)
+map[row][column] = "X"
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
