1d3e38fe72012888f8efd1f19fcf4b564d2c761f
Sam <samuel.waters@gmail.com>
2023-03-16 10:27:33 -0600

    chore:day-5-lab-3

--- day-5/main.py
+++ day-5/main.py
@@ -40,6 +40,15 @@ for x in range(1, 101):
 print(count)
 
 
+# lab 3
+#Write your code below this row 👇
+total = 0
+for x in range(0, 101, 2):
+    total += x
+print(total)
+
+
+
 
 
 
---
+++ day-5/proj.py
@@ -0,0 +1,5 @@
+count = 0
+for x in range(1, 101):
+    count += x
+
+print(count)
