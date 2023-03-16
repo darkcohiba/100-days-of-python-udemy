ad27f233d2bc9f53790f80d9323bb6216e23ba18
Sam <samuel.waters@gmail.com>
2023-03-16 10:16:57 -0600

    chore:day-5-lab-2

--- day-5/main.py
+++ day-5/main.py
@@ -2,3 +2,42 @@ fruits = ["Apple", "Peach", "Banana"]
 
 for fruit in fruits:
     print(fruit)
+
+# lab 1
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+#Write your code below this row 👇
+
+total_heights = sum(student_heights)
+total_kids = len(student_heights)
+average = round(total_heights / total_kids)
+print(average)
+
+# lab 2
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+highest_score = 0
+# print(max(student_scores))
+for x in student_scores:
+    if x > highest_score:
+        highest_score = x
+print(f"The highest score in the class is: {highest_score}")
+
+
+
+
+
+
+
+
+
+
