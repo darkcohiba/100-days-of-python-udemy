1a405acc9eadd64f210977ccea6b0a8183be0beb
Sam <samuel.waters@gmail.com>
2023-03-17 13:25:44 -0600

    chore:day-6 reeborg 2

--- day-6/main.py
+++ day-6/main.py
@@ -5,3 +5,38 @@ while x < 5:
     print(x) #action
     x += 1 #increment
 print(sum)
+
+# Reeborgs World Hurdle 1
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+for x in range(6):
+    jump()
+
+# Hurdle 2
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() != True:
+    jump()
