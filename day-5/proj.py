1a5a7542b8b741a3bd8b08c4b44b7f2e8d57141c
Sam <samuel.waters@gmail.com>
2023-03-17 23:06:33 -0600

    chore:made project day-5 repeating

--- day-5/proj.py
+++ day-5/proj.py
@@ -33,3 +33,37 @@ for x in range(1, nr_numbers + 1):
 password = list(password)
 random.shuffle(password)
 print("".join(password))
+
+
+
+
+# replaying
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+
+start = 0
+while start == 0:
+    nr_letters = int(input("How many letters would you like in your password?\n"))
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+    password = ""
+    # print(random.choice(letters))
+    for x in range(1, nr_letters + 1):
+        password += random.choice(letters)
+    for x in range(1, nr_symbols + 1):
+        password += random.choice(symbols)
+    for x in range(1, nr_numbers + 1):
+        password += random.choice(numbers)
+    password = list(password)
+    random.shuffle(password)
+    print("".join(password))
+    choice = input("Do you need another password? (y/n): ")
+    if choice == "n":
+        start += 1
+        print("Thank you for using PyPassword Generator!")
