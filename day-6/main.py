81ec09f9b07409ea22596dcb7a3c1e9f95d776ae
Sam <samuel.waters@gmail.com>
2023-03-17 23:33:32 -0600

    chore:day-6 hurdle 4

--- day-6/main.py
+++ day-6/main.py
@@ -45,3 +45,39 @@ print(sum)
 # 
 # while not at_goal():
 #     jump()
+
+# hurdle 4
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     if wall_on_right():
+#         move()
+#     elif right_is_clear():
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()
+#     else:
+#         move()
+        
+# def base():
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         base()
+#     elif wall_in_front():
+#         jump()
+#     else:
+#         move()
+        
