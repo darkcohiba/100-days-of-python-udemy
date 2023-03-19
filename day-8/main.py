fe6a4f0023feeaae4c371770bb5680a1a9219b64
Sam <samuel.waters@gmail.com>
2023-03-19 15:32:23 -0600

    chore:day-8-lab-2

--- day-8/main.py
+++ day-8/main.py
@@ -20,3 +20,23 @@ paint_calc(height=test_h, width=test_w, cover=coverage)
 #     print(f"You'll need {round(num_of_cans)} cans of paint.")
 
 
+# lab 2
+#Write your code below this line 👇
+def prime_checker(number):
+    count = 0
+    # while prime == True:
+    if number <= 2:
+        print("It's a prime number.")
+    else:
+        for x in range(2, number):
+            if number % x == 0:
+                count += 1
+    if count == 0:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
