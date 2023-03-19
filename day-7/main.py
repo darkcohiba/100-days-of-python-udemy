69be03bc41bd066efe5431e12406f4ec445a62c5
Sam <samuel.waters@gmail.com>
2023-03-19 13:31:38 -0600

    chore:hangman corrections

--- day-7/main.py
+++ day-7/main.py
@@ -24,7 +24,7 @@ while word_length > 0:
     letter = input("Guess a letter: ").lower()
     if guessed_letters.count(letter) != 0:
         print("You have already guessed this letter! Guess a new letter!")
-        print(f"Words you have guessed: {guessed_letters}")
+        print(f"The letters you have guessed: {guessed_letters}")
     else:
         for each_letter in final_word:
             if letter == each_letter:
@@ -37,37 +37,38 @@ while word_length > 0:
                 # print("Wrong")
                 # print(display)
                 counter += 1
-        print(display)
+        print(f"Your updated word is: {display}")
         guessed_letters.append(letter)
         if display.count(letter) != 0:
-            print(f"Words you have guessed: {guessed_letters}")
             print(f"The letter {letter} was found!")
+            print(f"The letters you have guessed: {guessed_letters}")
         if display.count(letter) == 0:
-            print(f"Words you have guessed: {guessed_letters}")
             print(f"The letter {letter} was not found!")
+            print(f"The letters you have guessed: {guessed_letters}")
             lives -= 1
         if display.count("_") == 0:
             print("You Won! Congratulations")
             print(f"Your word was {final_word}")
             word_length = 0
-        if lives == 7:
+        if lives == 7 and display.count("_") != 0:
             print(full_hangman_headless)
-        elif lives == 6:
+        elif lives == 6 and display.count("_") != 0:
             print(full_hangman_minus_six)
-        elif lives == 5:
+        elif lives == 5 and display.count("_") != 0:
             print(full_hangman_minus_five)
-        elif lives == 4:
+        elif lives == 4 and display.count("_") != 0:
             print(full_hangman_minus_four)
         elif lives == 3:
             print(full_hangman_minus_three)
-        elif lives == 2:
+        elif lives == 2 and display.count("_") != 0:
             print(full_hangman_minus_two)
-        elif lives == 1:
+        elif lives == 1 and display.count("_") != 0:
             print(full_hangman_minus_one)
             print("One guess left!!")
-        elif lives == 0:
+        elif lives == 0 and display.count("_") != 0:
             print("You Lost!")
             print(full_hangman)
             print(f"Your word was {final_word}")
             word_length = 0
         counter = 0
+
