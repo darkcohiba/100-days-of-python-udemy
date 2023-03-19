acf2bcc4b4f7645682b23bcd01973a5bff8444cc
Sam <samuel.waters@gmail.com>
2023-03-19 01:02:59 -0600

    feat:hangman finished minus polish

--- day-7/main.py
+++ day-7/main.py
@@ -1,10 +1,7 @@
 import requests
 import random
-word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 from hangman import *
-
-
-
+word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
 word = str(random.choice(WORDS))
@@ -20,57 +17,57 @@ print(f"Your word is:\n {display}")
 counter = 0
 word_length = len(final_word)
 # print(word_length)
-print(final_word)
+# print(final_word)
 guessed_letters = []
 lives = 7
 while word_length > 0:
     letter = input("Guess a letter: ").lower()
-    guessed_letters.append(letter)
-    for each_letter in final_word:
-        if letter == each_letter:
-            # print("Right")
-            display[counter] = letter
-            counter += 1
-            # print(display)
-            word_length -= 1
-        else:
-            # print("Wrong")
-            # print(display)
-            counter += 1
-    print(display)
-    if display.count(letter) != 0:
+    if guessed_letters.count(letter) != 0:
+        print("You have already guessed this letter! Guess a new letter!")
         print(f"Words you have guessed: {guessed_letters}")
-        print(f"The letter {letter} was found!")
-    if display.count(letter) == 0:
-        print(f"Words you have guessed: {guessed_letters}")
-        print(f"The letter {letter} was not found!")
-        lives -= 1
-    if display.count("_") == 0:
-        print("You Won! Congratulations")
-        word_length = 0
-    if lives == 7:
-        print(full_hangman_headless)
-    elif lives == 6:
-        print(full_hangman_minus_six)
-    elif lives ==5:
-        print(full_hangman_minus_five)
-    elif lives ==4:
-        print(full_hangman_minus_four)
-    elif lives ==3:
-        print(full_hangman_minus_three)
-    elif lives ==2:
-        print(full_hangman_minus_two)
-    elif lives ==1:
-        print(full_hangman_minus_one)
-        print("One guess left!!")
-    elif lives ==0:
-        print("You Lost!")
-        print(full_hangman)
-        print(f"Your word was {final_word}")
-        word_length = 0
-    
-    counter = 0
-
-# print("\n")
-# print("Congratulations You Won!!!")
-# print(final_word)
+    else:
+        for each_letter in final_word:
+            if letter == each_letter:
+                # print("Right")
+                display[counter] = letter
+                counter += 1
+                # print(display)
+                word_length -= 1
+            else:
+                # print("Wrong")
+                # print(display)
+                counter += 1
+        print(display)
+        guessed_letters.append(letter)
+        if display.count(letter) != 0:
+            print(f"Words you have guessed: {guessed_letters}")
+            print(f"The letter {letter} was found!")
+        if display.count(letter) == 0:
+            print(f"Words you have guessed: {guessed_letters}")
+            print(f"The letter {letter} was not found!")
+            lives -= 1
+        if display.count("_") == 0:
+            print("You Won! Congratulations")
+            print(f"Your word was {final_word}")
+            word_length = 0
+        if lives == 7:
+            print(full_hangman_headless)
+        elif lives == 6:
+            print(full_hangman_minus_six)
+        elif lives == 5:
+            print(full_hangman_minus_five)
+        elif lives == 4:
+            print(full_hangman_minus_four)
+        elif lives == 3:
+            print(full_hangman_minus_three)
+        elif lives == 2:
+            print(full_hangman_minus_two)
+        elif lives == 1:
+            print(full_hangman_minus_one)
+            print("One guess left!!")
+        elif lives == 0:
+            print("You Lost!")
+            print(full_hangman)
+            print(f"Your word was {final_word}")
+            word_length = 0
+        counter = 0
