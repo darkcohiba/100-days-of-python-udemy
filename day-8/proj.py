be7a5ce46b82140b8148344994084b56adfbc37c
Sam <samuel.waters@gmail.com>
2023-03-20 08:02:03 -0600

    chore: made encode/decode into one function

--- day-8/proj.py
+++ day-8/proj.py
@@ -1,37 +1,59 @@
+from art import *
+
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q","r","s","t","u","v","w","x","y","z"]
-
 direction = input("Type 'encode' to encrypt and 'decode' to decrypt: \n")
-def encrypt(shift, text):
-    # for i in range(len(alphabet)):
-    #     print(alphabet[i])
-    #     pass
+text = input("Type your message: \n")
+shift = int(input("Type your shift number: \n"))
+def caesar(shift, text, direction):
     message = ""
-    for x in range(len(text)):
-        letter = text[x]
-        idx = alphabet.index(letter)
-        message += alphabet[idx + shift]
-    # started with message as an array so the below line made it back into a string.
-    # message = "".join(message)
-    print(f"The encoded text is {message}.")
-
-def decrpyt(shift, text):
-    message = ""
-    for x in range(len(text)):
-        letter = text[x]
-        idx = alphabet.index(letter)
-        message += alphabet[idx - shift]
-    # below line made it back into a string
-    # message = "".join(message)
-    print(f"The decoded text is {message}.")
+    if direction == "encode":
+        for x in range(len(text)):
+            letter = text[x]
+            idx = alphabet.index(letter)
+            message += alphabet[idx + shift]
+        print(f"The encoded text is {message}.")
+    elif direction == 'decode':
+        for x in range(len(text)):
+            letter = text[x]
+            idx = alphabet.index(letter)
+            message += alphabet[idx - shift]
+        print(f"The decoded text is {message}.")
+    else: 
+        print("Please choose a viable direction")
+caesar(shift, text, direction)
 
 
-if direction == "encode":
-    text = input("Type your message: \n")
-    shift = int(input("Type your shift number: \n"))
-    encrypt(shift, text)
-elif direction == "decode":
-    text = input("Type your message: \n")
-    shift = int(input("Type your shift number: \n"))
-    decrpyt(shift, text)
-else:
-    print("Please choose encode or decode direction.")
+# def encrypt(shift, text):
+#     # for i in range(len(alphabet)):
+#     #     print(alphabet[i])
+#     #     pass
+#     message = ""
+#     for x in range(len(text)):
+#         letter = text[x]
+#         idx = alphabet.index(letter)
+#         message += alphabet[idx + shift]
+#     # started with message as an array so the below line made it back into a string.
+#     # message = "".join(message)
+#     print(f"The encoded text is {message}.")
+
+# def decrpyt(shift, text):
+#     message = ""
+#     for x in range(len(text)):
+#         letter = text[x]
+#         idx = alphabet.index(letter)
+#         message += alphabet[idx - shift]
+#     # below line made it back into a string
+#     # message = "".join(message)
+#     print(f"The decoded text is {message}.")
+
+
+# if direction == "encode":
+#     text = input("Type your message: \n")
+#     shift = int(input("Type your shift number: \n"))
+#     encrypt(shift, text)
+# elif direction == "decode":
+#     text = input("Type your message: \n")
+#     shift = int(input("Type your shift number: \n"))
+#     decrpyt(shift, text)
+# else:
+#     print("Please choose encode or decode direction.")
