e13a8747a3f75d94deb37ccf5be33555d39695f8
Sam <samuel.waters@gmail.com>
2023-03-20 22:22:11 -0600

    chore:moving to day-9

--- day-8/proj.py
+++ day-8/proj.py
@@ -3,26 +3,38 @@ from art import *
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q","r","s","t","u","v","w","x","y","z"]
 print(welcome)
 print(title)
-direction = input("Type 'encode' to encrypt and 'decode' to decrypt: \n")
-text = input("Type your message: \n")
-shift = int(input("Type your shift number: \n"))
-def caesar(shift, text, direction):
-    message = ""
-    if direction == "encode":
-        for x in range(len(text)):
-            letter = text[x]
-            idx = alphabet.index(letter)
-            message += alphabet[idx + shift]
-        print(f"The encoded text is {message}.")
-    elif direction == 'decode':
-        for x in range(len(text)):
-            letter = text[x]
-            idx = alphabet.index(letter)
-            message += alphabet[idx - shift]
-        print(f"The decoded text is {message}.")
-    else: 
-        print("Please choose a viable direction")
-caesar(shift, text, direction)
+go = True
+while go:
+    direction = input("Type 'encode' to encrypt and 'decode' to decrypt: \n")
+    text = input("Type your message: \n")
+    shift = int(input("Type your shift number: \n"))
+    def caesar(shift, text, direction):
+        message = ""
+        if direction == "encode":
+            for x in range(len(text)):
+                if text[x].isalpha():
+                    letter = text[x]
+                    idx = alphabet.index(letter)
+                    message += alphabet[idx + shift]
+                else:
+                    message += text[x]
+            print(f"The encoded text is {message}.")
+        elif direction == 'decode':
+            for x in range(len(text)):
+                if text[x].isalpha():
+                    letter = text[x]
+                    idx = alphabet.index(letter)
+                    message += alphabet[idx - shift]
+                else:
+                    message += text[x]
+            print(f"The decoded text is {message}.")
+        else: 
+            print("Please choose a viable direction")
+    caesar(shift, text, direction)
+    choice = input("Would you like to encode/decode again? (y/n): ")
+    if choice == "n":
+        print("Goodbye!")
+        go = False
 
 
 # def encrypt(shift, text):
@@ -59,3 +71,19 @@ caesar(shift, text, direction)
 #     decrpyt(shift, text)
 # else:
 #     print("Please choose encode or decode direction.")
+
+# solution from course
+
+# def ceasar_two(start_text, shift, direction):
+#     end_text = ""
+#     if direction == "decode":
+#         shift *= -1
+#     for char in start_text:
+#         # if char.isalpha():
+#             # or 
+#         if char in alphabet:
+#             position = alphabet.index(char)
+#             new_position = position + shift
+#             end_text = alphabet[new_position]
+#         else:
+#             end_text += char
