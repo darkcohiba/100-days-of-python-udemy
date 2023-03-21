89739b05c29254a70dbcc9a7da03502513afc39c
Sam <samuel.waters@gmail.com>
2023-03-21 08:10:59 -0600

    chore: day-9-lab-2

--- day-9/main.py
+++ day-9/main.py
@@ -22,3 +22,36 @@ for key in student_scores:
 # 🚨 Don't change the code below 👇
 print(student_grades)
 
+# lab 2
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+def add_new_country(country, visits, cities):
+    travel_log.append( {
+        "country": country,
+        "visits": visits,
+        "cities": cities
+    })
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+
