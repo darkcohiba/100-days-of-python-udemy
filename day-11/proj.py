f54644a48089c7f92907d4e1a62968b1ccf7b610
Sam <samuel.waters@gmail.com>
2023-03-22 10:36:18 -0600

    chore:day 10 project started

---
+++ day-11/proj.py
@@ -0,0 +1,6 @@
+from art import *
+
+print(blackjack)
+play = input("Do you want to play a game of Blackjack? (y/n): ")
+cards = {2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"}
+print(cards[0])
