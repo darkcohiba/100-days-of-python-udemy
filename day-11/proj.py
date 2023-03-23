b16bde47f6f691d7f3522972f884e5977356cdb8
Sam <samuel.waters@gmail.com>
2023-03-23 07:20:15 -0600

    chore: following solution

--- day-11/proj.py
+++ day-11/proj.py
@@ -11,50 +11,51 @@ computer_total = 0
 hit_me = True
 playing = True
 
-def deal(user_total, computer_total):
-    for x in range(2):
-        user_hand.append(random.choice(cards))
-        user_total += int(user_hand[x])
-        computer_hand.append(random.choice(cards))
-        computer_total += int(computer_hand[x])
-    check_if_over(user_total, computer_total)
+# def deal(user_total, computer_total):
+#     for x in range(2):
+#         user_hand.append(random.choice(cards))
+#         user_total += int(user_hand[x])
+#         computer_hand.append(random.choice(cards))
+#         computer_total += int(computer_hand[x])
+#     check_if_over(user_total, computer_total)
 
-def check_if_over(user_total, computer_total):
-    if computer_total == 21:
-        print("Computer has 21! User Lost")
-        playing = False
-    elif user_total == 21:
-        print("User has 21! Computer Lost")
-        playing = False
-    else:
-        print(user_hand)
-        print([computer_hand[0]])
-        print(computer_total)
-        print(user_total)
-        playing = False
+# def check_if_over(user_total, computer_total):
+#     if computer_total == 21:
+#         print("Computer has 21! User Lost")
+#         playing = False
+#     elif user_total == 21:
+#         print("User has 21! Computer Lost")
+#         playing = False
+#     else:
+#         print(user_hand)
+#         print([computer_hand[0]])
+#         print(computer_total)
+#         print(user_total)
+#         playing = False
 
-def check_if_over_single(total, player):
-    if total == 21:
-        print(f"{player} has 21! User Lost")
-        playing = False
-    else:
-        print(player)
-        print(total)
-        playing = False
+# def check_if_over_single(total, player):
+#     if total == 21:
+#         print(f"{player} has 21! User Lost")
+#         playing = False
+#     else:
+#         print(player)
+#         print(total)
+#         playing = False
 
-def hit_me(total, player):
-    player.append(random.choice(cards))
-    total += int(player[-1])
-    check_if_over_single(total, player)
+# def hit_me(total, player):
+#     player.append(random.choice(cards))
+#     total += int(player[-1])
+#     check_if_over_single(total, player)
 
-while playing:
-    deal(user_total, computer_total)    
-    add_card_choice = input("Would you like to hit? (yes/no) ")
-    if add_card_choice == 'yes':
-        hit_me(user_total, user_hand)
+# while playing:
+#     deal(user_total, computer_total)    
+#     add_card_choice = input("Would you like to hit? (yes/no) ")
+#     if add_card_choice == 'yes':
+#         hit_me(user_total, user_hand)
     
-    playing = False
-    user_total = 0
+#     playing = False
+#     user_total = 0
+
 
 
 
