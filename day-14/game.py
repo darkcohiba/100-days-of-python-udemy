2cacc9754141b7c0f4449c24ab1a1ccb0eb44897
Sam <samuel.waters@gmail.com>
2023-03-25 18:56:17 -0600

    chore: day 14 game done

--- day-14/game.py
+++ day-14/game.py
@@ -5,12 +5,54 @@ from art import logo, vs
 
 def get_random():
     return random.choice(data)
+
+def format_data(data):
+    name = data["name"]
+    description = data["description"]
+    country = data["country"]
+    # print(f'{name}: {data["follower_count"]}')
+    return f"{name}, a {description}, from {country}"
+
+def compare_followers(guess, follower_one, follower_two):
+    # print(f'{guess} {follower_one} {follower_two}')
+
+    if follower_one > follower_two:
+        return guess == "a"
+    elif follower_two > follower_one:
+        return guess == "b"
+
+
+
 def game():
     print(logo)
     score = 0
+    game_over = False
     choice_1 = get_random()
     choice_2 = get_random()
-    print(choice_1)
+    while not game_over:
+        choice_1 = choice_2
+        choice_2 = get_random()
+
+        while choice_1 == choice_2:
+            choice_2 = get_random()
+
+        print(f"Compare A: {format_data(choice_1)}")
+        print(vs)
+        print(f"Compare B: {format_data(choice_2)}")
+
+        guess = input("Who has more followers, A or B: \n").lower()
+        choice_1_followers = choice_1["follower_count"]
+        choice_2_followers = choice_2["follower_count"]
+        is_correct = compare_followers(guess, choice_1_followers, choice_2_followers)
+        
+
+        # print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_over = True
+            print(f"Sorry, that's wrong. Final score: {score}")
 
 
 game()
