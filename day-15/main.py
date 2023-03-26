36c981c7395b2c38fe7e7a179339b19654abe255
Sam <samuel.waters@gmail.com>
2023-03-25 21:18:43 -0600

    feat:i think i love pycharm

--- day-15/main.py
+++ day-15/main.py
@@ -3,7 +3,17 @@ from drinks import MENU, resources
 
 def add_money(money):
     resources["money"] += money
+
+
 def report():
     return f"Water: {resources['water']}ml \nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: $ {resources['money']}"
 
 
+in_shop = True
+while in_shop:
+    choice = input("What would you like? (espresso, cappuccino, latte): ")
+
+    if choice == "report":
+        print(report())
+    elif choice == "quit":
+        in_shop = False
