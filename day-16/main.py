82d3ab566a940b10b788e94158439ca885012a70
Sam <samuel.waters@gmail.com>
2023-03-26 18:52:39 -0600

    chore: day 16 finished;

--- day-16/main.py
+++ day-16/main.py
@@ -5,25 +5,19 @@ from money_machine import MoneyMachine
 # print(Menu().get_items())
 
 
-def coffee_maker():
-    in_shop = True
-    while in_shop:
-        robust = CoffeeMaker()
-        register = MoneyMachine()
-        choice = input("What would you like? (espresso, cappuccino, latte): ")
-        drink = Menu().find_drink(choice)
+robust = CoffeeMaker()
+register = MoneyMachine()
+in_shop = True
+while in_shop:
 
-        if choice == "report":
-            robust.report()
-            register.report()
-        elif choice == "quit":
-            in_shop = False
-        elif drink:
-            if robust.is_resource_sufficient(drink):
-                paid = register.make_payment(drink.cost)
-                if paid:
-                    robust.make_coffee(drink)
+    choice = input("What would you like? (espresso, cappuccino, latte): ")
+    drink = Menu().find_drink(choice)
 
-
-
-coffee_maker()
+    if choice == "report":
+        robust.report()
+        register.report()
+    elif choice == "quit":
+        in_shop = False
+    elif drink:
+        if robust.is_resource_sufficient(drink) and register.make_payment(drink.cost):
+            robust.make_coffee(drink)
