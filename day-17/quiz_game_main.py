8ef309192a29ed0b23d42faafdfa69fc469a4803
Sam <samuel.waters@gmail.com>
2023-03-28 08:37:23 -0600

    chore: started quiz brain third method

--- day-17/quiz_game_main.py
+++ day-17/quiz_game_main.py
@@ -1,5 +1,6 @@
 from data import question_data as data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -7,4 +8,7 @@ for question in data:
     new_q = Question(question["text"], question["answer"])
     question_bank.append(new_q)
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
