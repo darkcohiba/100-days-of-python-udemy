38a8c3d134c16a4216234e52a1b8eb2c72a9fdaf
Sam <samuel.waters@gmail.com>
2023-03-29 17:11:00 -0600

    chore:added import

--- different_imports.py
+++ different_imports.py
@@ -10,4 +10,16 @@
 #
 # tim = Turtle()
 
+# import turtle as t
+#
+# tim = t.Turtle()
 
+
+# from turtle import Turtle as T
+# from turtle import Screen as S
+#
+# tim = T()
+# tim.shape("circle")
+#
+# screen = S()
+# screen.exitonclick()
