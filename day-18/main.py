d109483b22de7977902f0b9810ca35cb19b86071
Sam <samuel.waters@gmail.com>
2023-03-29 20:15:20 -0600

    chore: fourth challenge day 18

--- day-18/main.py
+++ day-18/main.py
@@ -17,6 +17,105 @@ stt.shape("turtle")
 #     stt.forward(random.randint(15, 175))
 #     stt.left(random.randint(15, 175))
 
+# challenge 3 shape in shape
+# sides = 3
+# colors = ["red", "yellow", "blue", "pink", "lime green", "spring green", "indigo", "maroon", "saddle brown"]
+# for _ in range(9):
+#     angle = 360 / sides
+#     color = random.choice(colors)
+#     colors.remove(color)
+#     stt.pencolor(color)
+#     for _ in range(sides):
+#         stt.forward(100)
+#         stt.right(angle)
+#     sides += 1
+#     print(color)
+
+# challenge 4 random path
+# choice = ["left", "right"]
+# colors = ["red", "yellow", "blue", "pink", "lime green", "spring green", "indigo", "maroon", "saddle brown"]
+
+# stt.width(10)
+# for _ in range(10):
+#     color = random.choice(colors)
+#     stt.color(color)
+#     stt.forward(25)
+#     if random.randint(0, 1):
+#         stt.right(90)
+#     else:
+#         stt.left(90)
+
+# challenge 5 dot painting
+
+colors = ["red", "yellow", "blue", "pink", "lime green", "spring green", "indigo", "maroon", "saddle brown"]
+stt.width(10)
+
+
+def space():
+    stt.penup()
+    stt.forward(20)
+
+
+def dot():
+    stt.pendown()
+    stt.forward(10)
+
+
+def move_a_line():
+    for _ in range(10):
+        dot()
+        space()
+        dot()
+
+
+def turn_right():
+    stt.right(90)
+
+
+def turn_left():
+    stt.left(90)
+
+
+def move_up_line_right():
+    turn_right()
+    space()
+    turn_right()
+
+
+def move_up_line_left():
+    turn_left()
+    space()
+    turn_left()
+
+
+def set_color():
+    color = random.choice(colors)
+    stt.color(color)
+
+
+def set_posit(x, y):
+    stt.penup()
+    stt.setposition(x, y)
+    stt.pendown()
+
+
+set_posit(-200, -200)
+
+def cycle_left():
+    move_a_line()
+    set_color()
+    move_up_line_left()
+    set_color()
+
+def cycle_right():
+    move_a_line()
+    set_color()
+    move_up_line_right()
+    set_color()
+
+for _ in range(40):
+    cycle_left()
+    cycle_right()
 
 
 screen = Screen()
