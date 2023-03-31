963cb3959913b37d9d1d5f8eefd38c7c9d97eeee
Sam <samuel.waters@gmail.com>
2023-03-30 18:46:00 -0600

    chore: day 19 up to turtle race

--- day-19/main.py
+++ day-19/main.py
@@ -21,37 +21,40 @@ screen = Screen()
 
 
 # turtle race
-turtle_names = ["sam", "yese", "ste", "dav", "matt", "cryp", "leslie", "esy", "grape", "leo", "bark"]
-colors = ["red", "yellow", "blue", "pink", "lime green", "spring green", "indigo", "maroon", "saddle brown", "black"]
+# turtle_names = ["sam", "yese", "ste", "dav", "matt", "cryp", "leslie", "esy", "grape", "leo", "bark"]
+# colors = ["red", "yellow", "blue", "pink", "lime green", "spring green", "indigo", "maroon", "saddle brown", "black"]
+#
+# final_turtles = []
+#
+# def start_race(name):
+#     speed = random.randint(0, 9)
+#     name.speed(speed)
+#     name.forward(500)
+# screen.listen()
+# def set_up(turts):
+#     starting_x = -200
+#     starting_y = -200
+#     for _ in range(turts):
+#         name = random.choice(turtle_names)
+#         color = random.choice(colors)
+#         colors.remove(color)
+#         turtle_names.remove(name)
+#
+#         name = Turtle()
+#         name.penup()
+#         name.shape("turtle")
+#         name.color(color)
+#         name.setposition(starting_x, starting_y)
+#         # screen.onkey(key="space", fun=lambda: name.forward(100))
+#
+#         starting_y += 40
+#         final_turtles.append(name)
+#
+#
+# set_up(10)
 
-final_turtles = []
+# angelica solution
 
-def start_race(name):
-    speed = random.randint(0, 9)
-    name.speed(speed)
-    name.forward(500)
-
-def set_up(turts):
-    starting_x = -200
-    starting_y = -200
-    for _ in range(turts):
-        name = random.choice(turtle_names)
-        color = random.choice(colors)
-        colors.remove(color)
-        turtle_names.remove(name)
-
-        name = Turtle()
-        name.penup()
-        name.shape("turtle")
-        name.color(color)
-        name.setposition(starting_x, starting_y)
-
-        starting_y += 40
-        final_turtles.append(name)
-
-set_up(10)
-for turt in final_turtles:
-    start_race(turt)
 
 
 
