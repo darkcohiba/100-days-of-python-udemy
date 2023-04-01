36fd335fb10e763e7f37b10577b1388eed567513
Sam <samuel.waters@gmail.com>
2023-03-31 22:36:44 -0600

    chore: day 19 up to turtle race angelica solution

--- day-19/main.py
+++ day-19/main.py
@@ -55,19 +55,40 @@
 
 # angelica solution
 from turtle import Turtle, Screen
-
-colors_avail = ["green", "tan", "brown", "pink", "yellow", "blue"]
+import random
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
+colors_avail = ["green", "tan", "brown", "pink", "yellow", "blue"]
 user_bet = screen.textinput(title="make your bet", prompt=f"Which turtle will win the race, enter a color,\n {colors_avail}")
-print(user_bet)
+# print(user_bet)
+all_turtles = []
 y_var = [-70, -40, -10, 20, 50, 80]
+
 for turtle_index in range(0, 6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors_avail[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors_avail[turtle_index])
     # tim.speed("fastest")
-    tim.penup()
-    tim.goto(x=-230, y=y_var[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_var[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+            is_race_on = False
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 
 
