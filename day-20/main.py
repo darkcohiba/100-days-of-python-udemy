089e4f721be5fc3a594d3eb7fe3528583cae8b98
Sam <samuel.waters@gmail.com>
2023-04-02 14:02:37 -0600

    chore: day 20 snake building base

--- day-20/main.py
+++ day-20/main.py
@@ -5,6 +5,14 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+x_position = 0
+y_position = 0
+for _ in range(3):
+    new_turtle = Turtle()
+    new_turtle.shape("square")
+    new_turtle.color("white")
+    new_turtle.setposition(x_position, y_position)
+    x_position -= 20
 
 
 
