274cf8cca0be50939e51056bd0167d7a91c57319
Sam <samuel.waters@gmail.com>
2023-04-02 22:06:04 -0600

    chore: day 20 snake building base

--- day-20/main.py
+++ day-20/main.py
@@ -11,11 +11,19 @@ snakes = []
 for _ in range(3):
     new_turtle = Turtle(shape="square")
     new_turtle.color("white")
+    new_turtle.penup()
     new_turtle.setposition(x_position, 0)
     x_position -= 20
     snakes.append(new_turtle)
 
+game_is_on = True
 
+while game_is_on:
+    for snake in snakes:
+        snake.forward(20)
+        print(snake.position()[0])
+        if snake.position()[0] > 320:
+            game_is_on = False
 
 
 
