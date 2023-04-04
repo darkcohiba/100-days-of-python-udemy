718b61ee9b8b8978762c3ee2bd83303ff80e8894
Sam <samuel.waters@gmail.com>
2023-04-04 08:41:09 -0600

    chore: day 20 snake building base

--- day-20/main.py
+++ day-20/main.py
@@ -14,10 +14,10 @@ snake = Snake()
 
 
 screen.listen()
-screen.onkey(snake.up(), "Up")
-screen.onkey(snake.down(), "Down")
-screen.onkey(snake.left(), "Left")
-screen.onkey(snake.right(), "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
