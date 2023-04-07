f80b97a50fe79f68534da92781c5332b74398784
Sam <samuel.waters@gmail.com>
2023-04-07 07:46:01 -0600

    chore:day 22 breaking down steps

---
+++ day-22/main.py
@@ -0,0 +1,9 @@
+# TODO:
+#     - create the screen
+#     - create and move the paddle
+#     -create another paddle
+#     -create and move the ball
+#     -detect collision with wall and create bounce
+#     -detect collision with paddle
+#     -detect when paddle misses
+#     -keep score
