a12481e0d5fda2c2f5d830d675c5904e0ff9f540
Sam <samuel.waters@gmail.com>
2023-04-13 07:19:12 -0600

    chore: day 24

---
+++ day-24/main.py
@@ -0,0 +1,21 @@
+# using files and writing to text files
+# basic
+# file = open("file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+
+# best practice
+# with open("file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# replace text on file
+# with open("file.txt", mode="w") as file:
+#     file.write("help.")
+
+# append text to the file
+with open("file.txt", mode="a") as file:
+    file.write("second help.")
