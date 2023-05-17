e93ec90b693683a25e34042878f8a23eb08676f4
Sam <samuel.waters@gmail.com>
2023-05-17 08:00:57 -0600

    chore: day 25

--- day-25/main.py
+++ day-25/main.py
@@ -5,13 +5,19 @@
 #     for day in contents:
 #         weather_data.append(day)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as f:
+#     data = csv.reader(f)
+#     temperatures = []
+#     for row in data:
+#         print(row[1])
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as f:
-    data = csv.reader(f)
-    temperatures = []
-    for row in data:
-        print(row[1])
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+import pandas
+data = pandas.read_csv("weather_data.csv")
+print(data)
+print(data["temp"])
+
