cc11363ee829c00049eb93dc02f0e629e6724aba
Sam <samuel.waters@gmail.com>
2023-05-22 20:10:09 -0600

    chore: day 25 finish us game

--- day-25/us-game/main.py
+++ day-25/us-game/main.py
@@ -1,3 +1,4 @@
+import pandas
 import turtle
 
 screen = turtle.Screen()
@@ -5,11 +6,43 @@ screen.title("U.S States Guessing Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+state_data = pandas.read_csv('50_states.csv')
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
 
-turtle.onscreenclick(get_mouse_click_coor)
+# my solution
+# list_of_states = list(state_data.state)
+# while len(list_of_states) > 0:
+#
+#     # state_data[state_data.state == f"{answer_state}"]
+#     # print(list(state_data.state))
+#     if answer_state in list_of_states:
+#         print(state_data[state_data.state == f"{answer_state}"])
+#         list_of_states.remove(answer_state)
+#     else:
+#         print(f"No State matchs {answer_state}")
+#     print(list_of_states)
 
-turtle.mainloop()
+# angela solution
+all_states = state_data.state.to_list()
+guessed_states = []
+while len(all_states) > 0:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
+    answer_state = answer_state.title()
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state = state_data[state_data.state == f"{answer_state}"]
+        t.goto(int(state.x), int(state.y))
+        # t.write(answer_state)
+        t.write(state.state.item())
+        guessed_states.append(answer_state)
+        all_states.remove(answer_state)
+
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
 # screen.exitonclick()
