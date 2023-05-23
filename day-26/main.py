816a18ae36668fd55d54912009e0d083c0541f90
Sam <samuel.waters@gmail.com>
2023-05-23 07:50:58 -0600

    chore: day 26 base

--- day-26/main.py
+++ day-26/main.py
@@ -1,3 +1,16 @@
 # list comprehension
 # new_list = [new_item for item in list]
 
+old_list = [1, 2, 3]
+times_2 = [n * 2 for n in old_list]
+# print(times_2)
+names = ["sam", "nInfa", "adam"]
+correct_names = [n.title() for n in names]
+# print(correct_names)
+
+# list comprehension with strings
+name = "Sam"
+new_name = [letter for letter in name]
+print(new_name)
+
+# working with ranges
