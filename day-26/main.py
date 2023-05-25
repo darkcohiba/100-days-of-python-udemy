ba307263df612db82b0b7016a9b2c108b64611f7
Sam <samuel.waters@gmail.com>
2023-05-25 13:06:27 -0600

    chore: day 26 dict comprehensions

--- day-26/main.py
+++ day-26/main.py
@@ -38,6 +38,6 @@ print(even_num)
 students = ["Sam", "Max", "Alex", "Smith"]
 from random import randint as ri
 student_grades = {item:ri(50, 99) for item in students}
-print(student_grades)
+# print(student_grades)
 students_passed = {student:score for (student, score) in student_grades.items() if score > 70}
-print(students_passed)
+# print(students_passed)
