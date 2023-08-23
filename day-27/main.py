585e6cba37b1421d7e21782549602659af9c1323
Sam Waters <samuel.waters@gmail.com>
2023-08-23 13:32:02 -0600

    day 27: Other Tkinter Widgets: Radiobuttons, Scales, Checkbuttons and more

--- day-27/main.py
+++ day-27/main.py
@@ -20,6 +20,8 @@ first_label.pack()
 def button_click():
     print("clicked!")
     first_label["text"] = input.get()
+    # print what is in the
+    print(text.get("1.0", tkinter.END))
 
 
 button = tkinter.Button(text="Click Me!", command=button_click)
@@ -32,7 +34,23 @@ input = tkinter.Entry(width=20)
 input.pack()
 
 # text box
-text = tkinter.Text()
+text = tkinter.Text(height=10, width=50)
+text.insert(tkinter.END, "Example.")
+# text.focus()
+text.pack()
+
+#
+def radio_used():
+    print(radio_state.get())
+    first_label["text"] = radio_state.get()
+    
+#Variable to hold on to which radio button value is checked.
+radio_state = tkinter.IntVar()
+radiobutton1 = tkinter.Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = tkinter.Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
 #
 # numbers = [10, 12, 13, 15, 20]
 #
