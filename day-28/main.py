f7fcb66dd40947d47ebcbc5932e727a1afac8ee7
Sam Waters <samuel.waters@gmail.com>
2023-09-29 15:38:27 -0600

    day 28:  Challenge - Complete the Applications User Interface (UI)

--- day-28/main.py
+++ day-28/main.py
@@ -22,7 +22,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-title_label = Label(text="Timer", fg=GREEN, bg=YELLOW)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -32,6 +32,13 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 112, text="00:00", fill="black", font=(FONT_NAME, "35", "bold"))
 canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0, bd=0)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", highlightthickness=0, bd=0)
+reset_button.grid(column=2, row=2)
+
+
 
 
 
