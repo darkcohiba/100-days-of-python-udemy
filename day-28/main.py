20abf7ca62d05e7b42fac77be870369689303eb2
Sam Waters <samuel.waters@gmail.com>
2023-09-30 15:40:16 -0600

    day 28:  count down mechanism

--- day-28/main.py
+++ day-28/main.py
@@ -18,6 +18,7 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     print(count)
+    canvas.itemconfig(timer_text, text=count)
     if count > 0:
         window.after(1000, count_down, count -1)
 
@@ -25,7 +26,6 @@ def count_down(count):
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-count_down(5)
 
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
@@ -35,8 +35,10 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 112, text="00:00", fill="black", font=(FONT_NAME, "35", "bold"))
+timer_text = canvas.create_text(100, 112, text="00:00", fill="black", font=(FONT_NAME, "35", "bold"))
 canvas.grid(column=1, row=1)
+count_down(5)
+
 
 start_button = Button(text="Start", highlightthickness=0, bd=0)
 start_button.grid(column=0, row=2)
