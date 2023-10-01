f72052d813cbeca98c7df91ed70ca833363b5696
Sam Waters <samuel.waters@gmail.com>
2023-09-30 21:44:17 -0600

    day 28:  count down mechanism

--- day-28/main.py
+++ day-28/main.py
@@ -1,7 +1,6 @@
 import math
 from tkinter import *
 
-
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,12 +10,27 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_time():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+    if reps % 8 == 0:
+        print("long break min")
+        count_down(LONG_BREAK_MIN * 60)
+    elif reps % 2 == 0:
+        print("short break min")
+        count_down(SHORT_BREAK_MIN * 60)
+    else:
+        print("work min")
+        count_down(WORK_MIN * 60)
+
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     # format time from chatgpt
@@ -35,7 +49,8 @@ def count_down(count):
     # print(count)
     # canvas.itemconfig(timer_text, text=count)
     if count > 0:
-        window.after(1000, count_down, count -1)
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -53,18 +68,13 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 112, text="25:00", fill="black", font=(FONT_NAME, "35", "bold"))
 canvas.grid(column=1, row=1)
 
-
 start_button = Button(text="Start", highlightthickness=0, bd=0, command=start_time)
 start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0, bd=0)
 reset_button.grid(column=2, row=2)
 
-
 check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
-
-
-
 
 # keep the window open
 window.mainloop()
