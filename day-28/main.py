f9077ec9dfce4ab339d4662ad17c6cc3d45e790f
Sam Waters <samuel.waters@gmail.com>
2023-10-01 17:53:22 -0600

    day 28: Adding Checkmarks and Resetting the Application

--- day-28/main.py
+++ day-28/main.py
@@ -10,7 +10,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-reps = 0
+reps = 1
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
@@ -18,7 +18,6 @@ reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_time():
     global reps
-    reps += 1
     if reps > 8:
         pass
     if reps % 8 == 0:
@@ -28,18 +27,17 @@ def start_time():
     elif reps % 2 == 0:
         print("short break min")
         title_label.config(text="Break", fg=PINK)
-
         count_down(SHORT_BREAK_MIN * 60)
     else:
         print("work min")
         title_label.config(text="Work", fg=GREEN)
-
         count_down(WORK_MIN * 60)
-
+    reps += 1
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    # text = "✅",
     # format time from chatgpt
     # minutes = count // 60
     # seconds = count % 60
@@ -58,7 +56,12 @@ def count_down(count):
     if count > 0:
         window.after(1000, count_down, count - 1)
     else:
+        global reps
+        checks = reps // 2
+        print(checks)
+        check_marks.config(text=f"{checks * '✅'}")
         start_time()
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -82,7 +85,7 @@ start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0, bd=0)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 # keep the window open
