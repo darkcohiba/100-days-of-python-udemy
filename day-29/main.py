b112fa43c6f9d6191c65e36e02a86f03fde0ac50
Sam Waters <samuel.waters@gmail.com>
2023-10-01 20:45:09 -0600

    day 29: base

--- day-29/main.py
+++ day-29/main.py
@@ -1,5 +1,25 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg="yellow")
+
+
+canvas = Canvas(width=200, height=200, bg="yellow")
+canvas.pack()
+
+logo = PhotoImage(file="logo.png")
+
+canvas.create_image(100, 100, image=logo)
+
+
+
+# keep the window open
+window.mainloop()
