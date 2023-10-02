a637a452e6476dad23d8c8b17245007bd7671c5b
Sam Waters <samuel.waters@gmail.com>
2023-10-02 16:02:03 -0600

    day 29: Solution to the Creating the Grid Layout

--- day-29/main.py
+++ day-29/main.py
@@ -9,26 +9,26 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manger")
-window.config(padx=20, pady=20, bg="yellow")
+window.config(padx=20, pady=20, bg="white")
 
 
-canvas = Canvas(width=200, height=200, bg="yellow")
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
 canvas.grid(row=0, column=1)
 
 logo = PhotoImage(file="logo.png")
 
 canvas.create_image(100, 100, image=logo)
 
-website_label = Label(text="Website", bg="yellow", fg="black")
-email_label = Label(text="Email/Username", bg="yellow", fg="black")
-password_label = Label(text="password", bg="yellow", fg="black")
+website_label = Label(text="Website", bg="white", fg="black")
+email_label = Label(text="Email/Username", bg="white", fg="black")
+password_label = Label(text="password", bg="white", fg="black")
 website_label.grid(row=1, column=0)
 email_label.grid(row=2, column=0)
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=35)
-email_entry = Entry(width=35)
-password_entry = Entry(width=21)
+website_entry = Entry(width=35, fg="black", bg="white")
+email_entry = Entry(width=35, fg="black", bg="white")
+password_entry = Entry(width=21, fg="black", bg="white")
 website_entry.grid(row=1, column=1)
 email_entry.grid(row=2, column=1)
 password_entry.grid(row=3, column=1)
