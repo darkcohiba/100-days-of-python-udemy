202525dde2ecd08a89308d86dd2d85c2a58f0adb
Sam Waters <samuel.waters@gmail.com>
2023-10-03 16:43:24 -0600

    day 29: Challenge 3 - Saving Data to File

--- day-29/main.py
+++ day-29/main.py
@@ -28,20 +28,20 @@ email_label.grid(row=2, column=0)
 password_label.grid(row=3, column=0)
 
 # input fields
-website_entry = Entry(width=35, fg="black", bg="white")
+website_entry = Entry(width=35, fg="black", bg="white", highlightthickness=0, highlightbackground="white")
 website_entry.grid(row=1, column=1, columnspan=2)
 
-email_entry = Entry(width=35, fg="black", bg="white")
+email_entry = Entry(width=35, fg="black", bg="white", highlightthickness=0, highlightbackground="white")
 email_entry.grid(row=2, column=1, columnspan=2)
 
-password_entry = Entry(width=21, fg="black", bg="white")
+password_entry = Entry(width=21, fg="black", bg="white", highlightthickness=0, highlightbackground="white")
 password_entry.grid(row=3, column=1)
 
 # buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", highlightthickness=0, highlightbackground="white")
 generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, highlightthickness=0, highlightbackground="white")
 add_button.grid(column=1, row=4, columnspan=2)
 
 # keep the window open
