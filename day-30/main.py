cc9d84ad8a9d980aaab17f362ee6b43e61852143
Sam Waters <samuel.waters@gmail.com>
2023-10-15 17:47:11 -0600

    day 30: [Interactive Coding Exercise] KeyError Handling

--- day-30/main.py
+++ day-30/main.py
@@ -13,3 +13,36 @@
 #     print(content)
 # finally:
 #     raise TypeError("Bad Type?")
+
+# interactive challenge
+#
+# fruits = eval(input())
+# # 🚨 Do not change the code above
+#
+# # TODO: Catch the exception and make sure the code runs without crashing.
+# def make_pie(index):
+#     try:
+#       fruit = fruits[index]
+#     except IndexError as e:
+#       print(f"Fruit pie")
+#     else:
+#       print(fruit + " pie")
+#
+#
+# # 🚨 Do not change the code below
+# make_pie(1)
+
+
+# second interactive challenge
+
+# eval() function will create a list of dictionaries using the input
+# facebook_posts = eval(input())
+#
+# total_likes = 0
+# # TODO: Catch the KeyError exception
+# for post in facebook_posts:
+#     try:
+#       total_likes = total_likes + post['Likes']
+#     except KeyError:
+#       total_likes = total_likes + 0
+# print(total_likes)
