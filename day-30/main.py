a56fbe05274ea9087597dff22719d42c485313cc
Sam Waters <samuel.waters@gmail.com>
2023-10-15 17:32:11 -0600

    day 30:Raising your own Exceptions

---
+++ day-30/main.py
@@ -0,0 +1,15 @@
+# handling exceptions
+# try:
+#     file = open("data.txt")
+#     a_dictionary = {"key":"value"}
+#     print(a_dictionary["sadf"])
+# except FileNotFoundError as e:
+#     file = open("data.txt", "w")
+#     file.write(f"Inside the exception!!... {e} ")
+# except KeyError as key:
+#     print(f'The key {key} does not exist')
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("Bad Type?")
