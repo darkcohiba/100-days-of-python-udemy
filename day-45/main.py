c87d59c1d25134790b9f6c60748db3f40953080a
Sam Waters <samuel.waters@gmail.com>
2023-10-26 08:17:06 -0600

    day 45 :Finding and Selecting Particular Elements with BeautifulSoup

--- day-45/main.py
+++ day-45/main.py
@@ -5,4 +5,20 @@ with open('website.html') as web_file:
     contents = web_file.read()
 
 soup = BeautifulSoup(contents, "html.parser")
-print(soup.title)
+# print(soup.title)
+# print(soup.title.string)
+# print(soup)
+# print(soup.prettify())
+# print(soup.li)
+# print(soup.a)
+
+
+# print(soup.find_all(name="li"))
+# print(soup.findAll(name="li"))
+
+all_anchor_tags = soup.findAll(name="a")
+for tag in all_anchor_tags:
+    print(tag.string)
+    print(tag.getText())
+    print(tag.href)
+    print(tag.get("href"))
